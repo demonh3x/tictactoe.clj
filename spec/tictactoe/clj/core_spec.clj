55039b2b271995a079fd4bc7a0e5604e9912068c
Mateu Adsuara <mateuadsuara@gmail.com>
2015-06-29 14:08:11 +0100

    can do-turn in a board

--- spec/tictactoe/clj/core_spec.clj
+++ spec/tictactoe/clj/core_spec.clj
@@ -2,8 +2,51 @@
   (:require [speclj.core :refer :all]
             [tictactoe.clj.core :refer :all]))
 
-(describe "game"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "next-mark"
+          (it "in a board with no marks, x is the next"
+               (should= :x
+                        (next-mark [:empty :empty :empty
+                                    :empty :empty :empty
+                                    :empty :empty :empty])))
+
+          (it "in a board with one x mark, o is the next"
+               (should= :o
+                        (next-mark [:empty :x     :empty
+                                    :empty :empty :empty
+                                    :empty :empty :empty])))
+
+          (it "in a board with one x mark and one o mark, x is the next"
+               (should= :x
+                        (next-mark [:x     :o     :empty
+                                    :empty :empty :empty
+                                    :empty :empty :empty]))))
+
+(describe "do-turn"
+          (it "in the initial board, places the mark at player x's chosen space"
+              (should= [:x     :empty :empty
+                        :empty :empty :empty
+                        :empty :empty :empty]
+                       (do-turn (fn [] 0)
+                                [:empty :empty :empty
+                                 :empty :empty :empty
+                                 :empty :empty :empty])))
+
+          (it "in the initial board, places the mark at player x's chosen space"
+              (should= [:empty :x     :empty
+                        :empty :empty :empty
+                        :empty :empty :empty]
+                       (do-turn (fn [] 1)
+                                [:empty :empty :empty
+                                 :empty :empty :empty
+                                 :empty :empty :empty])))
+
+          (it "in a board with one move from x, places the mark at player o's chosen space"
+              (should= [:x     :o     :empty
+                        :empty :empty :empty
+                        :empty :empty :empty]
+                       (do-turn (fn [] 1)
+                                [:x     :empty :empty
+                                 :empty :empty :empty
+                                 :empty :empty :empty]))))
 
 (run-specs)
--- src/tictactoe/clj/core.clj
+++ src/tictactoe/clj/core.clj
@@ -1,1 +1,14 @@
 (ns tictactoe.clj.core)
+
+(defn- count-marks-in [board mark-type]
+  (count (filter #(= mark-type %) board)))
+
+(defn next-mark [board]
+  (let [marks-of-type (partial count-marks-in board)]
+    (if (< (marks-of-type :o) (marks-of-type :x))
+      :o
+      :x)))
+
+(defn do-turn [player board]
+  (let [chosen-space (player)]
+    (assoc-in board [chosen-space] (next-mark board))))
