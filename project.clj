c2c1b8d058752bc77a2f1c8d4930cc26386961fe
Mateu Adsuara <mateuadsuara@gmail.com>
2015-06-29 10:02:28 +0100

    Initial project structure

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject tictactoe.clj "0.1.0-SNAPSHOT"
+  :description "a tictactoe implementation"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
+  :plugins [[speclj "3.3.1"]]
+  :test-paths ["spec"])
---
+++ spec/tictactoe/clj/core_spec.clj
@@ -0,0 +1,9 @@
+(ns tictactoe.clj.core-spec
+  (:require [speclj.core :refer :all]
+            [tictactoe.clj.core :refer :all]))
+
+(describe "game"
+  (it "FIXME, I fail."
+    (should= 0 1)))
+
+(run-specs)
