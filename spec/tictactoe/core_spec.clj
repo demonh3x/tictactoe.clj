a469241b086606c4dc2a2af4eaae1d167d9ebd31
Mateu Adsuara <mateuadsuara@gmail.com>
2015-06-29 16:42:29 +0100

    Can detect winner in a board

--- spec/tictactoe/core_spec.clj
+++ spec/tictactoe/core_spec.clj
@@ -40,4 +40,58 @@
                                  :empty :empty :empty]
                                 (fn [] 1)))))
 
+(describe "marks-at-lines"
+          (it "are the marks at each line"
+              (should= [[:o     :x     :empty]
+                        [:empty :o     :empty]
+                        [:x     :x     :empty]
+                        [:o     :empty :x]
+                        [:x     :o     :x]
+                        [:empty :empty :empty]
+                        [:o     :o     :empty]
+                        [:empty :o     :x]]
+                       (marks-at-lines [:o     :x :empty
+                                        :empty :o :empty
+                                        :x     :x :empty]))))
+
+(describe "same-mark?"
+          (it "is true when there are three x marks"
+              (should= true
+                       (same-mark? [:x :x :x])))
+
+          (it "is true when there are three o marks"
+              (should= true
+                       (same-mark? [:o :o :o])))
+
+          (it "is false when there are two x marks and one o mark"
+              (should= false
+                       (same-mark? [:x :x :o])))
+
+          (it "is false when there are two o marks and one empty space"
+              (should= false
+                       (same-mark? [:empty :o :o])))
+
+          (it "is false when all are empty"
+              (should= false
+                       (same-mark? [:empty :empty :empty]))))
+
+(describe "winner"
+          (it "is none in a board with no marks"
+              (should= :none
+                       (winner [:empty :empty :empty
+                                :empty :empty :empty
+                                :empty :empty :empty])))
+
+          (it "is x in a board with the first horizontal line occupied by x"
+              (should= :x
+                       (winner [:x     :x     :x
+                                :empty :empty :empty
+                                :empty :empty :empty])))
+
+          (it "is o in a board with the third vertical line occupied by o"
+              (should= :o
+                       (winner [:empty :empty :o
+                                :empty :empty :o
+                                :empty :empty :o]))))
+
 (run-specs)
--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -12,3 +12,23 @@
 (defn do-turn [board player]
   (let [chosen-space (player)]
     (assoc-in board [chosen-space] (next-mark board))))
+
+(defn- marks-at [board line]
+  (map #(nth board %) line))
+
+(defn marks-at-lines [board]
+  (let [lines [[0 1 2] [3 4 5] [6 7 8]
+               [0 3 6] [1 4 7] [2 5 8]
+               [0 4 8] [2 4 6]]]
+    (map #(marks-at board %) lines)))
+
+(defn same-mark? [marks]
+  (every? #(and (not= :empty %)
+                (= (first marks) %) )
+          marks))
+
+(defn winner [board]
+  (let [lines (marks-at-lines board)
+        winning-lines (filter same-mark? lines)
+        winner (ffirst winning-lines)]
+    (or winner :none)))
