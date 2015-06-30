180aef130f604aa939bae2914dd644699a9bae2c
Mateu Adsuara <mateuadsuara@gmail.com>
2015-06-30 16:27:19 +0100

    Adds human-player

--- spec/tictactoe/cli_spec.clj
+++ spec/tictactoe/cli_spec.clj
@@ -26,3 +26,59 @@
                        (render [:x     :o     :empty
                                 :empty :empty :empty
                                 :empty :empty :empty]))))
+
+(describe "human-player!"
+          (around [it] (with-out-str (it)))
+
+          (it "returns 0 when 0 is received from *in*"
+              (should= 0
+                       (with-in-str "0\n"
+                                    (human-player! [:empty :empty :empty
+                                                    :empty :empty :empty
+                                                    :empty :empty :empty]))))
+
+          (it "returns 1 when 1 is received from *in* after receiving something that is not a number"
+              (should= 1
+                       (with-in-str "not-a-number\n1\n"
+                                    (human-player! [:empty :empty :empty
+                                                    :empty :empty :empty
+                                                    :empty :empty :empty]))))
+
+          (it "tells *out* invalid input after receiving something that is not a number"
+              (should-contain "That is not a number!\n"
+                              (with-out-str
+                                (with-in-str "not-a-number\n1\n"
+                                             (human-player! [:empty :empty :empty
+                                                             :empty :empty :empty
+                                                             :empty :empty :empty])))))
+
+          (it "returns 1 when 1 is received from *in* after receiving a space that is not empty"
+              (should= 1
+                       (with-in-str "0\n1\n"
+                                    (human-player! [:x     :empty :empty
+                                                    :empty :empty :empty
+                                                    :empty :empty :empty]))))
+
+          (it "tells *out* space not empty after receiving a space that is not empty"
+              (should-contain "That space is not empty!\n"
+                              (with-out-str
+                                (with-in-str "0\n1\n"
+                                             (human-player! [:x     :empty :empty
+                                                             :empty :empty :empty
+                                                             :empty :empty :empty])))))
+
+          (it "asks *out* to choose an empty space in an empty board"
+              (should-contain "It is your turn! Choose one space: (0 1 2 3 4 5 6 7 8)\n"
+                              (with-out-str
+                                (with-in-str "0\n"
+                                             (human-player! [:empty :empty :empty
+                                                             :empty :empty :empty
+                                                             :empty :empty :empty])))))
+
+          (it "asks *out* to choose an empty space in a board with only two empty spaces"
+              (should-contain "It is your turn! Choose one space: (7 8)\n"
+                              (with-out-str
+                                (with-in-str "7\n"
+                                             (human-player! [:x     :o     :x
+                                                             :x     :o     :x
+                                                             :o     :empty :empty]))))))
--- src/tictactoe/cli.clj
+++ src/tictactoe/cli.clj
@@ -1,4 +1,5 @@
-(ns tictactoe.cli)
+(ns tictactoe.cli
+  (:require [tictactoe.core :refer :all]))
 
 (defn- render-mark [mark]
   (case mark
@@ -16,3 +17,32 @@
 
 (defn render [board]
   (to-string (map add-newline (render-rows board))))
+
+(defn- read-int! []
+  (try
+    (Integer/parseInt (read-line))
+    (catch NumberFormatException e
+      (println "That is not a number!")
+      (read-int!))))
+
+(defn- print-to-choose-from [empty-spaces]
+  (println "It is your turn! Choose one space:" (pr-str empty-spaces)))
+
+(defn- ask-for [empty-space?]
+  (let [choice (read-int!)]
+    (when-not (empty-space? choice)
+      (println "That space is not empty!"))
+    choice))
+
+(defn- first [predicate seq]
+  (some predicate seq))
+
+(defn- ask-until-is-one-of [empty-spaces]
+  (let [empty-space? (fn [space]
+                       ((set empty-spaces) space))
+        choices (repeatedly #(ask-for empty-space?))]
+    (print-to-choose-from empty-spaces)
+    (first empty-space? choices)))
+
+(defn human-player! [board]
+  (ask-until-is-one-of (empty-spaces board)))
