c49dbebc7506645b4828ddd91c6adc8c35975ba1
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-06 17:43:21 +0100

    Simplifies main

--- src/tictactoe/main.clj
+++ src/tictactoe/main.clj
@@ -1,18 +1,13 @@
 (ns tictactoe.main
   (:require [tictactoe.cli :refer [human-player! print-board print-outcome]]
-            [tictactoe.board :refer [finished? winner]]
+            [tictactoe.board :refer [winner]]
             [tictactoe.game :refer [players game]]
             [tictactoe.ai :refer [computer-player]]))
-
-(defn display [board]
-  (print-board board)
-  (println)
-  (when (finished? board)
-    (print-outcome (winner board))))
 
 (defn -main [& args]
   (let [player-types  {"human"    human-player!
                        "computer" computer-player}
         players       (apply players (map player-types args))
         board-history (game players)]
-    (doall (map display board-history))))
+    (doall (map print-board board-history))
+    (print-outcome (winner (last board-history)))))
